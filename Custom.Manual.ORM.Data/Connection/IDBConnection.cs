f8bc6e5cde350aaf93239caf4c8ad4b0d3451a97
David Allan Ribeiro <profissional@david-allan.com.br>
2018-02-07 00:10:29 -0200

    Created IDBConnection and IConnectionSetting interfaces to .Data project

---
+++ Custom.Manual.ORM.Data/Connection/IDBConnection.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.SqlClient;
+using Custom.Manual.ORM.Data.Setting;
+
+namespace Custom.Manual.ORM.Data.Connection
+{
+    public interface IDBConnection
+    {
+        IConnectionSetting Settings { get; }
+        SqlConnection Connection();
+        void SetCommand(string sql);
+        object SetInsertCommand(string sql);
+        bool SetUpdateCommand(string sql);
+        SqlDataReader Result();
+    }
+}
---
+++ Custom.Manual.ORM.Data/Setting/IConnectionSetting.cs
@@ -0,0 +1,12 @@
+﻿namespace Custom.Manual.ORM.Data.Setting
+{
+    public interface IConnectionSetting
+    {
+        string Database { get; set; }
+        string Password { get; set; }
+        string Server { get; set; }
+        string User { get; set; }
+
+        bool HasEncryption { get; set; }
+    }
+}

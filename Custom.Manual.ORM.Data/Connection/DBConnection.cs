c61e1d9179081fc712bdf00cdf34367be6808f2d
David Allan Ribeiro <profissional@david-allan.com.br>
2018-02-07 00:11:18 -0200

    Implemented the IDBConnection and IConnectionSetting interfaces to

---
+++ Custom.Manual.ORM.Data/Connection/DBConnection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data.SqlClient;
+using Custom.Manual.ORM.Data.Setting;
+
+namespace Custom.Manual.ORM.Data.Connection
+{
+    public class DBConnection : IDBConnection
+    {
+        private SqlCommand _sqlCommand;
+        private SqlConnection _sqlConnection;
+        public IConnectionSetting Settings { get; private set; }
+
+        public DBConnection(IConnectionSetting connectionSetting)
+        {
+            Settings = connectionSetting ?? throw new InvalidOperationException("");
+
+            var connectionString = @"Data Source=" + Settings.Server + ";" +
+                         "Initial Catalog=" + Settings.Database + ";" +
+                         "User id=" + Settings.User + ";" +
+                         "Password=" + Settings.Password + ";";
+
+            _sqlConnection = new SqlConnection(connectionString);
+            _sqlConnection.Open();
+            _sqlConnection.Close();
+        }
+
+        public SqlConnection Connection() => _sqlConnection;
+
+        public SqlDataReader Result() => _sqlCommand.ExecuteReader();
+
+        public void SetCommand(string sql)
+        {
+            _sqlCommand = new SqlCommand(sql, _sqlConnection);
+            _sqlCommand.Connection.Open();
+        }
+
+        public object SetInsertCommand(string sql)
+        {
+            _sqlCommand = new SqlCommand(sql, _sqlConnection);
+            return _sqlCommand.ExecuteScalar();
+        }
+
+        public bool SetUpdateCommand(string sql)
+        {
+            _sqlCommand = new SqlCommand(sql, _sqlConnection);
+            return _sqlCommand.ExecuteNonQuery() != 0;
+        }
+    }
+}
---
+++ Custom.Manual.ORM.Data/Setting/ConnectionSetting.cs
@@ -0,0 +1,11 @@
+﻿namespace Custom.Manual.ORM.Data.Setting
+{
+    public class ConnectionSetting : IConnectionSetting
+    {
+        public string Database { get; set; }
+        public string Password { get; set; }
+        public string Server { get; set; }
+        public string User { get; set; }
+        public bool HasEncryption { get; set; }
+    }
+}

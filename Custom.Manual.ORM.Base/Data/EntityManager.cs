f0d0b564e0dd2dc65c03d13bf05c94f222be6ac3
David Allan Ribeiro <profissional@david-allan.com.br>
2018-02-10 07:44:27 -0200

    Changed constructor od EntityManager to have a IDBConnection on constructor

--- Custom.Manual.ORM.Base/Data/EntityManager.cs
+++ Custom.Manual.ORM.Base/Data/EntityManager.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using Custom.Manual.ORM.Base.Interfaces;
+using Custom.Manual.ORM.Data.Connection;
 
 namespace Custom.Manual.ORM.Base.Data
 {
     public abstract class EntityManager<T, TId> : IEntityManager<T, TId> where T : IIdentifiable<TId>
     {
+        private readonly IDBConnection _dbConnection;
+
+        public EntityManager(IDBConnection dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+
         public TId Add(T entity)
         {
             throw new NotImplementedException();

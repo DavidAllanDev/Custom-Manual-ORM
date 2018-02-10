cad63eb35eed3aca94c92df7ec4801aff1b41234
David Allan Ribeiro <profissional@david-allan.com.br>
2018-02-10 07:46:05 -0200

    Added project data.repository to define the repositories on a business context

---
+++ Custom.Manual.ORM.Data.Repositories/Context/ICustomBusinessPersistenceDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Custom.Manual.ORM.Data.Repositories.Context
+{
+    interface ICustomBusinessPersistenceDbContext
+    {
+    }
+}

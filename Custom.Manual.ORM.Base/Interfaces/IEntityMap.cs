8f4f63e8acc7d700e17bba149686223bfd716fc6
David Allan Ribeiro <profissional@david-allan.com.br>
2018-02-10 07:43:31 -0200

    Created an interface IEntityMap to define how mappers will do it

---
+++ Custom.Manual.ORM.Base/Interfaces/IEntityMap.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Custom.Manual.ORM.Base.Interfaces
+{
+    public interface IEntityMap
+    {
+        string EntityTableName();
+        Dictionary<string, string> EntityMapper();
+    }
+}

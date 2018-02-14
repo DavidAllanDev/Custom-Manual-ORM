ad3cc22edc4edf43a6be882788691ee8b3795adb
David Allan Ribeiro <profissional@david-allan.com.br>
2018-02-14 12:33:01 -0200

    Add and implemented a DemoMap

--- Custom.Manual.ORM.Domain/DomainClasses/DemoClass.cs
+++ Custom.Manual.ORM.Domain/DomainClasses/DemoClass.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Custom.Manual.ORM.Base.Interfaces;
+﻿using Custom.Manual.ORM.Base.Interfaces;
 
 namespace Custom.Manual.ORM.Domain.DomainClasses
 {
     public class DemoClass : IIdentifiable<int>
     {
-        public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Id { get; set; }
     }
 }
---
+++ Custom.Manual.ORM.Domain/DomainMapping/DemoMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Custom.Manual.ORM.Base.Data.Fields;
+using Custom.Manual.ORM.Base.Interfaces;
+
+namespace Custom.Manual.ORM.Domain.DomainMapping
+{
+    public class DemoMap : IEntityMap
+    {
+        public Dictionary<string, string> EntityMapper()
+        {
+            return new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+            {
+                {KeyFields.Id, "DemoMapSQLTableName_intId"}
+            };
+        }
+
+        public string EntityTableName() => "DemoMapSQLTableName";
+    }
+}

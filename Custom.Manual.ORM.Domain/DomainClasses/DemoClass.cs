c4d602be5be95a87d09bd5c98d10e2e662e069cd
David Allan Ribeiro <profissional@david-allan.com.br>
2018-02-14 11:43:09 -0200

    Added a DemoClass example of a Entity class

---
+++ Custom.Manual.ORM.Domain/DomainClasses/DemoClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Custom.Manual.ORM.Base.Interfaces;
+
+namespace Custom.Manual.ORM.Domain.DomainClasses
+{
+    public class DemoClass : IIdentifiable<int>
+    {
+        public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}

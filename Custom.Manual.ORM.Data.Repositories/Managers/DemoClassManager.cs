bcfcfc0a40ec62568e5eb8ddb1e942b54e29a84b
David Allan Ribeiro <profissional@david-allan.com.br>
2018-02-14 15:29:10 -0200

    Added and implemented a DemoClassManager

---
+++ Custom.Manual.ORM.Data.Repositories/Managers/DemoClassManager.cs
@@ -0,0 +1,18 @@
+﻿using Custom.Manual.ORM.Base.Data;
+using Custom.Manual.ORM.Data.Connection;
+using Custom.Manual.ORM.Domain.DomainClasses;
+using Custom.Manual.ORM.Base.Interfaces;
+using Custom.Manual.ORM.Domain.DomainMapping;
+
+namespace Custom.Manual.ORM.Data.Repositories.Managers
+{
+    public class DemoClassManager : EntityManager<DemoClass, int>
+    {
+        public DemoClassManager(IDBConnection dbConnection) : base(dbConnection)
+        {
+            IEntityMap mapper = new DemoMap();
+            TableName = mapper.EntityTableName();
+            ExplicitMappings = mapper.EntityMapper();
+        }
+    }
+}

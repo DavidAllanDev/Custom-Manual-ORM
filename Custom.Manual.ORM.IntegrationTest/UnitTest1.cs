749f0967a82f3211928a4cce0aacebf825e99df5
David Allan Ribeiro <david@david-allan.co>
2018-02-15 15:05:15 -0200

    Created Custom.Manual.ORM.IntegrationTest project

---
+++ Custom.Manual.ORM.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Custom.Manual.ORM.IntegrationTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

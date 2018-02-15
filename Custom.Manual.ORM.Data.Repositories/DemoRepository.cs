f35d2cb5ae329c0df47dc3db8b3b723595dd318d
David Allan Ribeiro <david@david-allan.co>
2018-02-15 14:57:23 -0200

    Created DemoRepository
    Created DemoRepository tha may be able to provide all domain modeled as needed in a context

---
+++ Custom.Manual.ORM.Data.Repositories/DemoRepository.cs
@@ -0,0 +1,16 @@
+﻿using Custom.Manual.ORM.Data.Connection;
+using Custom.Manual.ORM.Data.Repositories.Context;
+using Custom.Manual.ORM.Data.Repositories.Managers;
+
+namespace Custom.Manual.ORM.Data.Repositories
+{
+    public class DemoRepository : ICustomBusinessPersistenceDbContext
+    {
+        public DemoRepository(IDBConnection dbConnection)
+        {
+            DemoClasses = new DemoClassManager(dbConnection);
+        }
+
+        public DemoClassManager DemoClasses { get; set; }
+    }
+}

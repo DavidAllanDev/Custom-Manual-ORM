24203ac8fce576798a875d5fe551594ce7d9b01d
David Allan Ribeiro <david@david-allan.co>
2018-02-22 15:21:31 -0300

    Added Namr property to DemoClass entity

--- Custom.Manual.ORM.Domain/DomainClasses/DemoClass.cs
+++ Custom.Manual.ORM.Domain/DomainClasses/DemoClass.cs
@@ -5,5 +5,6 @@ namespace Custom.Manual.ORM.Domain.DomainClasses
     public class DemoClass : IIdentifiable<int>
     {
         public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
--- Custom.Manual.ORM.Domain/DomainMapping/DemoMap.cs
+++ Custom.Manual.ORM.Domain/DomainMapping/DemoMap.cs
@@ -10,6 +10,7 @@ namespace Custom.Manual.ORM.Domain.DomainMapping
         public Dictionary<string, string> EntityMapper() => new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
             {
                 {KeyFields.Id, "DemoMapSQLTableName_intId"},
+                {"Name" ,"DemoMapSQLTableName_strName"}
             };
 
         public string EntityTableName() => "DemoMapSQLTableName";

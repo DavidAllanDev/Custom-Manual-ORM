84afea0f9daf3a15e971683ba55ba43354e692ec
David Allan Ribeiro <david@david-allan.co>
2018-02-23 14:02:17 -0300

    Created ICache interface

---
+++ Custom.Manual.ORM.Cache/Model/ICache.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Custom.Manual.ORM.Cache.Model
+{
+    public interface ICache<T> where T : class
+    {
+        void Add(T entity);
+        void AddRange(List<T> entity);
+        void Update(T entity);
+        void UpdateRange(List<T> entity);
+        T Get(string id);
+        List<T> GetRange(int page, int size);
+        List<T> GetAll();
+        void Remove(string id);
+        void Remove(T entity);
+        void RemoveRange(List<T> entity);
+        void Clear();
+        int Estimate();
+    }
+}

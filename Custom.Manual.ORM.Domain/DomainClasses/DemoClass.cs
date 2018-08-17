dfa112596a9bcd8cf9a63e046a7f46e0e4c1b6a0
David Allan <david@david-allan.co>
2018-08-17 15:22:23 -0300

    Added data annotation to DemoClass
    Added data annotation to DemoClass as an example, in this moment the Entity Manager is no mapping it based on it and not even the SQLBase class

--- Custom.Manual.ORM.Domain/DomainClasses/DemoClass.cs
+++ Custom.Manual.ORM.Domain/DomainClasses/DemoClass.cs
@@ -1,4 +1,5 @@
 ﻿using Custom.Manual.ORM.Base.Interfaces;
+using Custom.Manual.ORM.Base.Data.Fields;
 
 namespace Custom.Manual.ORM.Domain.DomainClasses
 {
@@ -6,5 +7,12 @@ namespace Custom.Manual.ORM.Domain.DomainClasses
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        /// <summary>
+        /// On this example we have a data Column Name that differ from the property of the class
+        /// On the next versions of this ORM it will be getting it from this data annotation
+        /// </summary>
+        [ColumnName(Name = "Enable")]
+        public bool Active { get; set; }
     }
 }
